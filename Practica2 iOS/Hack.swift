aaf09ab199e4342829ec62687988dadaf696f6aa
AlexCuevasDev <43845457+AlexCuevasDev@users.noreply.github.com>
2018-11-09 16:51:30 +0100

    hacked

--- Practica2 iOS/Hack.swift
+++ Practica2 iOS/Hack.swift
@@ -19,7 +19,9 @@ class Hack: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
+        if(insertText.text != "")
+            hacking()
+            
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -32,6 +34,10 @@ class Hack: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
        return coinArray[row]
+    }
+    
+    func hacking(){
+        coinDictionary[pickerView.selectedRow()].value = insertText.text
     }
 
 }
