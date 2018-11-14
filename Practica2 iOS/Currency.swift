284297cf4aec933d3364244ef9ef2aabf7ad1950
Alex Cuevas <alu2018311@a22o05.local>
2018-11-14 20:11:14 +0100

    Persistance

--- Practica2 iOS/Currency.swift
+++ Practica2 iOS/Currency.swift
@@ -7,9 +7,31 @@
 //
 
 import Foundation
+import os.log
 
-class Currency: CustomStringConvertible{
-    public var description: String { return "\(name) \(symbol)" }
+class Currency: NSObject, NSCoding{
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: Data.dataName)
+        aCoder.encode(value, forKey: Data.dataValue)
+        aCoder.encode(symbol, forKey: Data.dataSymbol)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let nameCoder = aDecoder.decodeObject(forKey: Data.dataName) as? String else {
+            os_log("Unable to decode the name for the Currency object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+         guard let valueCoder = aDecoder.decodeObject(forKey: Data.dataValue) as? Double else {
+            os_log("Unable to decode the value for the Currency object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        guard let symbolCoder = aDecoder.decodeObject(forKey: Data.dataValue) as? Character else {
+            os_log("Unable to decode the value for the Currency object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        self.init(name: nameCoder,value: valueCoder,symbol: symbolCoder)
+    }
     
     var name : String
     var value : Double
@@ -20,4 +42,13 @@ class Currency: CustomStringConvertible{
         self.value = value
         self.symbol = symbol
     }
+    
+    struct Data {
+        static var dataName : String = "name"
+        static var dataValue : String = "value"
+        static var dataSymbol : String = "symbol"
+    }
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("coins")
 }
